cad1fa28d8b78e1f3defd120f436860c914458ee
theGeekkit <111545346+theGeekkit@users.noreply.github.com>
2023-05-11 16:37:10 -0500

    making onyx files api with jwt 5-11-23

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+resources :users, param: :_username
+post '/auth/login', to: 'authentication#login'
+get '/*a', to: 'application#not_found'
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
